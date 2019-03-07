a9a1e3521459d3d36396cb95884d148039508eef
SD SkyKlouD <sdsk@sdsk.one>
2019-03-07 23:31:58 +0900

    Add example configuration file

---
+++ config.example.js
@@ -0,0 +1,19 @@
+/*
+ *  Configurations for dust-on-me
+ *                 ⓒ SD SkyKlouD
+ */
+
+// Rename this file to `config.js` when you are done with filling these fields.
+
+module.exports = {
+    // Twitter API configurations (for module `twit`)
+    twitterConfigs: {
+        consumer_key:        "",    // Consumer Key
+        consumer_secret:     "",    // Consumer Secret
+        access_token:        "",    // Access Token
+        access_token_secret: ""     // Access Token Secret
+    },
+
+    // Airkorea UTF-8 URL encoded API Key (data.go.kr / openapi.airkorea.or.kr)
+    airkoreaApiKey: ""
+};
