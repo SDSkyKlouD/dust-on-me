b1010a4f02b564512669884b9beb46cf90c7d26b
SD SkyKlouD <sdsk@sdsk.one>
2019-03-08 22:58:21 +0900

    Logging / Add simple function to post Tweet

--- bot.js
+++ bot.js
@@ -5,10 +5,14 @@ const airKoreaKey = String.raw`${require("./config.js").airkoreaApiKey}`;
 const axios       = require("axios");
 
 /* Simple functions */
-const isUsableVar = function(obj) { return typeof(obj) !== "undefined" && obj !== null; }
+const logInfo = text => console.log("[I] " + text);
+const logError = text => console.error("[E] " + text);
+const logDebug = text => console.debug("[D] " + text);
+const isUsableVar = obj => typeof(obj) !== "undefined" && obj !== null;
+const postPublicTextTweet = text => twitter.post("statuses/update", { status: text });
 /* === */
 
-
+    
 /**
  * Call AirKorea API and returns the JSON response
  * 
@@ -31,24 +35,44 @@ async function callAirKoreaAPI(endpoint, options) {
     let apiParams = "";
     let finalAPIURL = "http://openapi.airkorea.or.kr/openapi/services/rest/" + endpoint + "?";
 
+    logInfo("Calling AirKorea API");
+    logDebug("Endpoint is `" + endpoint + "`, with options " + JSON.stringify(options));
+
     if(isUsableVar(options) && Object.keys(options).length !== 0) {
+        logDebug("Parameter `options` have something, start loop");
+
         for(let index in options) {
-            apiParams += index.toString() + "=" + options[index].toString() + "&";
+            var append = index.toString() + "=" + options[index].toString() + "&";
+
+            logDebug("options[" + index + "] = " + options[index] + " | will be appended to API call parameters as `" + append + "`");
+            apiParams += append
         }
     }
 
     finalAPIURL += encodeURI(apiParams + "_returnType=json&ServiceKey=") + airKoreaKey;
+    logDebug("Final AirKorea API request URL is `" + finalAPIURL + "`, start reqeust");
 
     try {
         let response = await axios.get(finalAPIURL);
-        
+
+        logDebug("Got something from API");
+
         if(isUsableVar(response) && isUsableVar(response.data)) {
+            logInfo("API has called and the response looks good");
+            logDebug("Returning `response.data`");
+
             return response.data;
         } else {
+            logError("Response is not usable. Failed to call API");
+            logDebug("Will return `undefined`");
+
             return undefined;
         }
     } catch(error) {
-        console.error(error);
+        logError("Something wrong while calling API.");
+        logError(error);
+        logDebug("API request failed, so will return `undefined`");
+
         return undefined;
     }
 }
