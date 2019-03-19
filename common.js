c2278ee060011c8d2b669584bb8e7eb0fcd1d169
SD SkyKlouD <sdsk@sdsk.one>
2019-03-19 22:53:54 +0900

    Add PM dust unit in common.js

--- common.js
+++ common.js
@@ -17,26 +17,29 @@ module.exports = {
     kmePM25YearlyBaseline: 15,     // Korea Ministry of Env - PM2.5 yearly baseline : 15㎍/㎥
     // Korea Ministry of Environment baseline enforced from 2018. 03. 27. / same baseline with USA, Japan, etc.
 
+    /* Units */
+    PMDustUnit : "㎍/㎥",
+
     /* Sido names */
     // Value: [_short name_, _official full name_, _keywords for mention command_...]
     sidoNamesKor: {
-        "busan":     ["부산", "부산광역시", "부산시", "부산직할시"],
-        "chungbuk":  ["충북", "충청북도", "충북도"],
-        "chungnam":  ["충남", "충청남도", "충남도"],
+        "seoul":     ["서울", "서울특별시", "서울시", "수도"],
+        "incheon":   ["인천", "인천광역시", "인천시", "인천직할시"],
         "daegu":     ["대구", "대구광역시", "대구시", "대구직할시"],
         "daejeon":   ["대전", "대전광역시", "대전시", "대전직할시"],
-        "gangwon":   ["강원", "강원도"],
         "gwangju":   ["광주", "광주광역시", "광주직할시"],            // No "광주시" - conflict with 광주시 in 경기도
-        "gyeongbuk": ["경북", "경상북도", "경북도"],
-        "gyeonggi":  ["경기", "경기도"],
-        "gyeongnam": ["경남", "경상남도", "경남도"],
-        "incheon":   ["인천", "인천광역시", "인천시", "인천직할시"],
+        "busan":     ["부산", "부산광역시", "부산시", "부산직할시"],
+        "ulsan":     ["울산", "울산광역시", "울산시", "울산직할시"],
+        "sejong":    ["세종", "세종특별자치시", "세종시", "세종특별시", "세종자치시"],
         "jeju":      ["제주", "제주특별자치도", "제주도", "제주특별도", "제주자치도"],
+        "gyeonggi":  ["경기", "경기도"],
+        "gangwon":   ["강원", "강원도"],
+        "gyeongbuk": ["경북", "경상북도", "경북도"],
+        "gyeongnam": ["경남", "경상남도", "경남도"],
+        "chungbuk":  ["충북", "충청북도", "충북도"],
+        "chungnam":  ["충남", "충청남도", "충남도"],
         "jeonbuk":   ["전북", "전라북도", "전북도"],
         "jeonnam":   ["전남", "전라남도", "전남도"],
-        "sejong":    ["세종", "세종특별자치시", "세종시", "세종특별시", "세종자치시"],
-        "seoul":     ["서울", "서울특별시", "서울시", "수도"],
-        "ulsan":     ["울산", "울산광역시", "울산시", "울산직할시"]
     },
     sidoNamesEng: {     // Space char will be ignored when processing a mention command
         "busan":     ["Busan", "Busan Metropolitan City",
