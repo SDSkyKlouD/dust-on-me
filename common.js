6b931154801167f8aa741b4271bc9ec0bfc547e5
SD SkyKlouD <sdsk@sdsk.one>
2019-03-19 11:39:12 +0900

    Add English sido names

--- common.js
+++ common.js
@@ -15,9 +15,10 @@ module.exports = {
     whoPM25YearlyBaseline: 10,     // WHO - PM2.5 yearly baseline                   : 10㎍/㎥
     kmePM25DailyBaseline:  35,     // Korea Ministry of Env - PM2.5 daily baseline  : 35㎍/㎥
     kmePM25YearlyBaseline: 15,     // Korea Ministry of Env - PM2.5 yearly baseline : 15㎍/㎥
+    // Korea Ministry of Environment baseline enforced from 2018. 03. 27. / same baseline with USA, Japan, etc.
 
     /* Sido names */
-    /* Value - [_short name_, _official full name_, _alternatives_...] */
+    // Value: [_short name_, _official full name_, _keywords for mention command_...]
     sidoNamesKor: {
         "busan":     ["부산", "부산광역시", "부산시", "부산직할시"],
         "chungbuk":  ["충북", "충청북도", "충북도"],
@@ -36,7 +37,78 @@ module.exports = {
         "sejong":    ["세종", "세종특별자치시", "세종시", "세종특별시", "세종자치시"],
         "seoul":     ["서울", "서울특별시", "서울시", "수도"],
         "ulsan":     ["울산", "울산광역시", "울산시", "울산직할시"]
+    },
+    sidoNamesEng: {     // Space char will be ignored when processing a mention command
+        "busan":     ["Busan", "Busan Metropolitan City",
+                      "BusanCity", "BusanSi", "Busan-si", "BusanGwangyeoksi", "BusanJikhalsi",
+                      "Pusan", "PusanMetropolitanCity",
+                      "PusanCity", "PusanSi", "Pusan-si", "PusanGwangyeoksi", "PusanJikhalsi"],
+        "chungbuk":  ["Chungbuk", "North Chungcheong Province",
+                      "Chungcheongbukdo", "Chungbuk-do", "Chungbukdo",
+                      "Chungcheongbuk-do", "ChungcheongbukProvince", "ChungbukProvince",
+                      "NorthChungcheongdo", "NorthChungcheong-do"],
+        "chungnam":  ["Chungnam", "South Chungcheong Province",
+                      "Chungcheongnamdo", "Chungnam-do", "Chungnamdo",
+                      "Chungcheongnam-do", "ChungcheongnamProvince", "ChungnamProvince",
+                      "SouthChungcheongdo", "SouthChungcheong-do"],
+        "daegu":     ["Daegu", "Daegu Metropolitan City",
+                      "DaeguCity", "DaeguSi", "Daegu-si", "DaeguGwangyeoksi", "DaeguJikhalsi",
+                      "Taegu", "TaeguMetropolitanCity",
+                      "TaeguCity", "TaeguSi", "Taegu-si", "TaeguGwangyeoksi", "TaeguJikhalsi"],
+        "daejeon":   ["Daejeon", "Daejeon Metropolitan City",
+                      "DaejeonCity", "DaejeonSi", "Daejeon-si", "DaejeonGwangyeoksi", "DaejeonJikhalsi",
+                      "Taejon", "TaejonMetropolitanCity",
+                      "TaejonCity", "TaejonSi", "Taejon-si", "TaejonGwangyeoksi", "TaejonJikhalsi"],
+        "gangwon":   ["Gangwon", "Gangwon Province",
+                      "Gangwondo", "Gangwon-do",
+                      "Kangwon", "Kangwon-do",
+                      "Kangwondo", "Kangwon Province"],
+        "gwangju":   ["Gwangju", "Gwangju Metropolitan City",
+                      "GwangjuCity", "GwangjuGwangyeoksi", "GwangjuJikhalsi",
+                      "Kwangju", "KwangjuMetropolitanCity",
+                      "KwangjuCity", "KwangjuGwangyeoksi", "KwangjuJikhalsi"],
+        "gyeongbuk": ["Gyeongbuk", "North Gyeongsang Province",
+                      "Gyeongsangbukdo", "Gyeongbuk-do", "Gyeongbukdo",
+                      "Gyeongsangbuk-do", "GyeongsangbukProvince", "GyeongbukProvince",
+                      "NorthGyeongsangdo", "NorthGyeongsang-do",
+                      "Kyeongbuk", "North Kyeongsang Province",
+                      "Kyeongsangbukdo", "Kyeongbuk-do", "Kyeongbukdo",
+                      "Kyeongsangbuk-do", "KyeongsangbukProvince", "KyeongbukProvince",
+                      "NorthKyeongsangdo", "NorthKyeongsang-do"],
+        "gyeonggi":  ["Gyeonggi", "Gyeonggi Province",
+                      "Gyeonggido", "Gyeonggi-do",
+                      "Kyeonggi", "Kyeonggi-do",
+                      "Kyeonggido", "Kyeonggi Province"],
+        "gyeongnam": ["Gyeongnam", "South Gyeongsang Province",
+                      "Gyeongsangnamdo", "Gyeongnam-do", "Gyeongnamdo",
+                      "Gyeongsangnam-do", "GyeongsangnamProvince", "GyeongnamProvince",
+                      "SouthGyeongsangdo", "SouthGyeongsang-do",
+                      "Kyeongnam", "South Kyeongsang Province",
+                      "Kyeongsangnamdo", "Kyeongnam-do", "Kyeongnamdo",
+                      "Kyeongsangnam-do", "KyeongsangnamProvince", "KyeongnamProvince",
+                      "SouthKyeongsangdo", "SouthKyeongsang-do"],
+        "incheon":   ["Incheon", "Incheon Metropolitan City",
+                      "IncheonCity", "IncheonSi", "Incheon-si", "IncheonGwangyeoksi", "IncheonJikhalsi",
+                      "Inchon", "InchonMetropolitanCity",
+                      "InchonCity", "InchonSi", "Inchon-si", "InchonGwangyeoksi", "InchonJikhalsi"],
+        "jeju":      ["Jeju", "Jeju Special Autonomous Province",
+                      "Jejudo", "Jeju-do", "JejuProvince", "JejuSpecialProvince",
+                      "JejuTeukbyeolJachido", "JejuTeukbyeolJachi-do", "JejuTeukbyeolJachiProvince"],
+        "jeonbuk":   ["Jeonbuk", "North Jeolla Province",
+                      "Jeollabukdo", "Jeonbuk-do", "Jeonbukdo",
+                      "Jeollabuk-do", "JeollabukProvince", "JeonbukProvince",
+                      "NorthJeollado", "NorthJeolla-do"],
+        "jeonnam":   ["Jeonnam", "South Jeolla Province",
+                      "Jeollanamdo", "Jeonnam-do", "Jeonnamdo",
+                      "Jeollanam-do", "JeollanamProvince", "JeonnamProvince",
+                      "SouthJeollado", "SouthJeolla-do"],
+        "sejong":    ["Sejong", "Sejong Special Autonomous City",
+                      "SejongCity", "SejongSi", "Sejong-si", "SejongSpecialCity",
+                      "SejongAutonomousCity", "SejongTeukbyeolJachisi", "SejongTeukbyeolJachi-si"],
+        "seoul":     ["Seoul", "Seoul Special City",
+                      "SeoulCity", "SeoulSi", "Seoul-si",
+                      "SeoulTeukbyeolsi", "SeoulTeukbyeol-si", "Sudo", "Capital", "CapitalCity"],
+        "ulsan":     ["Ulsan", "Ulsan Metropolitan City",
+                      "UlsanCity", "UlsanSi", "Ulsan-si", "UlsanGwangyeoksi", "UlsanJikhalsi"]
     }
 }
-
-// Korea Ministry of Environment baseline enforced from 2018. 03. 27. / same baseline with USA, Japan, etc.
